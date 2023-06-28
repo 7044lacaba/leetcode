fc2bfeb869e701f35ea2f4d4bfe686e1bfa8f5f2
7044lacaba <johnlacaba6@gmail.com>
2023-06-28 12:55:54 -0700

    2 my answer

--- Blind-75/1-Arrays-And-Hashing/2-Valid-Anagram.py
+++ Blind-75/1-Arrays-And-Hashing/2-Valid-Anagram.py
@@ -5,3 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        
+        s_copy = s
+        t_copy = t
+
+        for item in t_copy:
+            if item in s_copy:
+                s_copy = s_copy.replace(item, '', 1)
+            else:
+                return False
+        
+        if len(s_copy) != 0:
+            return False
+        return True 
