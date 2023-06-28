48f00b944d5092cbe87c80cfbe66a4f764ee8092
7044lacaba <johnlacaba6@gmail.com>
2023-06-28 00:19:42 -0700

    2 init

--- Blind-75/1-Arrays-And-Hashing/1-Contains-Duplicate.py
+++ Blind-75/1-Arrays-And-Hashing/1-Contains-Duplicate.py
@@ -5,8 +5,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+
+        # Time: O(N^2)
+        # Space: O(N)
+        # Solution I came up with
         copy = nums
         for i, item in enumerate(copy):
-            new_list = 
-            if 
+            copy.pop(i)
+            if item in copy:
+                return True
+        return False
+    
+        # Time: (nlogn)
+        # Space: O(1)
+        # Optimized solution
+
+        hashset = set()
+
+        for item in nums:
+            if item in hashset:
+                return True
+            hashset.add(item)
+        return False
+        
         
