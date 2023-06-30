01dc12ad54555064a1794813904969d9cc1172fa
7044lacaba <johnlacaba6@gmail.com>
2023-06-29 23:59:20 -0700

    4

--- Blind-75/1-Arrays-And-Hashing/test.py
+++ Blind-75/1-Arrays-And-Hashing/test.py
@@ -1,3 +1,17 @@
         # Time:
         # Space: 
         # Solution: 
+'''
+\begin{section}{Education}
+    \begin{subsectionnobullet}{Master of Procrastination}{Master's degree program}{University of Laziness}{May 2019 -- Present}
+        \italicitem{Thesis: The Art of Last-Minute Panic and Its Impact on Productivity}
+        \italicitem{Successfully procrastinated and panicked at the last moment}
+        \italicitem{Final Grade: 420/420}
+    \end{subsectionnobullet}
+    
+    \begin{subsectionnobullet}{Bachelor of Slack}{Bachelor's degree program}{Slackers University}{May 2015 -- August 2019}
+        \italicitem{Major: Procrastination Studies}
+        \italicitem{Final grade: 69/69}
+    \end{subsectionnobullet}
+\end{section}
+'''
