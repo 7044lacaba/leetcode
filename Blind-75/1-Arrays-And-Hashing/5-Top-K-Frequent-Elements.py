3a7578386d79a5752138702714ef1172ef15b18e
7044lacaba <johnlacaba6@gmail.com>
2023-07-04 16:18:32 -0700

    5 first solution

--- Blind-75/1-Arrays-And-Hashing/5-Top-K-Frequent-Elements.py
+++ Blind-75/1-Arrays-And-Hashing/5-Top-K-Frequent-Elements.py
@@ -5,4 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        dict = {}
+        list_val = [0] * k
+        list_key = [0] * k
         
+        for num in nums:
+            dict[num] = dict.get(num, 0) + 1
+        
+        for item in dict:
+            for i, spot, in enumerate(list_val):
+                if dict[item] > list_val[i]:
+                    list_val[i] = dict[item]
+                    list_key[i] = item
+                    break
+        return list_key
--- Blind-75/1-Arrays-And-Hashing/test.py
+++ Blind-75/1-Arrays-And-Hashing/test.py
@@ -1,17 +1,24 @@
-        # Time:
-        # Space: 
-        # Solution: 
-'''
-\begin{section}{Education}
-    \begin{subsectionnobullet}{Master of Procrastination}{Master's degree program}{University of Laziness}{May 2019 -- Present}
-        \italicitem{Thesis: The Art of Last-Minute Panic and Its Impact on Productivity}
-        \italicitem{Successfully procrastinated and panicked at the last moment}
-        \italicitem{Final Grade: 420/420}
-    \end{subsectionnobullet}
-    
-    \begin{subsectionnobullet}{Bachelor of Slack}{Bachelor's degree program}{Slackers University}{May 2015 -- August 2019}
-        \italicitem{Major: Procrastination Studies}
-        \italicitem{Final grade: 69/69}
-    \end{subsectionnobullet}
-\end{section}
-'''
+
+
+nums = [4,4,1,1,1,1,2,2,2,3]
+
+
+nums = sorted(nums)
+
+print(nums)
+
+
+dict = {}
+        
+for num in nums:
+    dict[num] = dict.get(num, 0) + 1
+
+
+print(dict)
+k = 2
+list = [0] * k
+print(list)
+
+#for item in dict:
+#    for spot in list:
+#        if dict[item] > spot
