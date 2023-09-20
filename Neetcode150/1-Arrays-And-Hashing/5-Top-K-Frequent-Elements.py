dabf420140d1850babcce7810f266b79825c3e0e
7044lacaba <johnlacaba6@gmail.com>
2023-09-20 14:28:00 -0700

    test

--- Neetcode150/1-Arrays-And-Hashing/5-Top-K-Frequent-Elements.py
+++ Neetcode150/1-Arrays-And-Hashing/5-Top-K-Frequent-Elements.py
@@ -39,3 +39,32 @@ class Solution(object):
             del dict[current]
 
         return final[:k]
+
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        dict = {}
+        list = []
+        for i in range(len(nums) + 1):
+            sub = []
+            list.append(sub)
+
+        for item in nums:
+            dict[item] = 1 + dict.get(item, 0)
+        for k, v in dict.items():
+            list[v].append(k)
+
+        print(dict)
+        print(list)
+        final = []
+        for i in range(len(list) - 1, 0, -1):
+            for n in list[i]:
+                final.append(item)
+                if len(final) == k:
+                    return final
--- Neetcode150/1-Arrays-And-Hashing/test.py
+++ Neetcode150/1-Arrays-And-Hashing/test.py
@@ -1,7 +1,25 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+nums = [1,1,1,2,2,3]
+k = 2
+
+
+dict = {}
+list = []
+for i in range(len(nums) + 1):
+    sub = []
+    list.append(sub)
+
+for item in nums:
+    dict[item] = 1 + dict.get(item, 0)
+for k, v in dict.items():
+    list[v].append(k)
+
+print(dict)
+print(list)
+final = []
+for i in range(len(list) - 1, 0, -1):
+    for n in list[i]:
+        final.append(n)
+        if len(final) == k:
+            break
+
+print(final)
