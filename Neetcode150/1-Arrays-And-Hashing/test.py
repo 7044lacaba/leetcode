a5330238ccc57a6836d52c0817dc923bd24761a3
7044lacaba <johnlacaba6@gmail.com>
2023-09-22 17:01:34 -0700

    6

--- Neetcode150/1-Arrays-And-Hashing/test.py
+++ Neetcode150/1-Arrays-And-Hashing/test.py
@@ -1,25 +1,9 @@
-nums = [1,1,1,2,2,3]
-k = 2
 
-
-dict = {}
-list = []
-for i in range(len(nums) + 1):
-    sub = []
-    list.append(sub)
-
-for item in nums:
-    dict[item] = 1 + dict.get(item, 0)
-for key, v in dict.items():
-    list[v].append(key)
-
-print(dict)
-print(list)
 final = []
-for i in range(len(list) - 1, 0, -1):
-    for n in list[i]:
-        final.append(n)
-        if len(final) == k:
-            break
-
-print(final)
+for i, item in enumerate(nums):
+    temp = nums
+    del temp[i]
+    value = 1
+    for item in temp:
+        value = value * item
+    final.append(value)
