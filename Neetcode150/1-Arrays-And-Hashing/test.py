9486ecfc0c689bd5407401845d8af38789ef3637
7044lacaba <johnlacaba6@gmail.com>
2023-09-22 17:14:39 -0700

    test

--- Neetcode150/1-Arrays-And-Hashing/test.py
+++ Neetcode150/1-Arrays-And-Hashing/test.py
@@ -1,9 +1,12 @@
+nums = [1,2,3,4]
 
 final = []
 for i, item in enumerate(nums):
-    temp = nums
-    del temp[i]
+    temp = nums.copy()
+    temp.pop(i)
     value = 1
     for item in temp:
         value = value * item
     final.append(value)
+
+print(final)
