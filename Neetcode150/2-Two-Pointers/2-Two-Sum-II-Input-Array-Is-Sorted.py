a0981e304f353f4b493a90efd206d989a7e3ad0f
7044lacaba <johnlacaba6@gmail.com>
2023-09-30 18:13:37 -0700

    2.2

--- Neetcode150/2-Two-Pointers/2-Two-Sum-II-Input-Array-Is-Sorted.py
+++ Neetcode150/2-Two-Pointers/2-Two-Sum-II-Input-Array-Is-Sorted.py
@@ -5,4 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        pass
+        index_1 = 0
+        index_2 = 1
+        dic = {}
+        while index_1 != index_2:
+            # Add first into dict
+            value = target - numbers[index_1]
+            dic[value] = index_1
+            # Check item at index 2
+            if numbers[index_2] in dic:
+                ret = [(dic[numbers[index_2]] + 1), (index_2 +1)]
+                return(ret)
+            # Increment
+            if index_2 + 1 == len(numbers):
+                index_1 += 1
+            else:
+                index_1 += 1
+                index_2 += 1
--- Neetcode150/2-Two-Pointers/test.py
+++ Neetcode150/2-Two-Pointers/test.py
@@ -1,1 +1,26 @@
-s = "A man, a plan, a canal: Panama"
+numbers = [2,3,4]
+target = 6
+
+index_1 = 0
+index_2 = 1
+
+dict = {}
+
+while index_1 != index_2:
+
+    # Add first into dict
+    value = target - numbers[index_1]
+    dict[value] = index_1
+
+    # Check item at index 2
+    if numbers[index_2] in dict:
+        ret = [(dict[numbers[index_2]] + 1), (index_2 +1)]
+        print(ret)
+    
+    # Increment
+    if index_2 + 1 == len(numbers):
+        index_1 += 1
+    else:
+        index_1 += 1
+        index_2 += 1
+
