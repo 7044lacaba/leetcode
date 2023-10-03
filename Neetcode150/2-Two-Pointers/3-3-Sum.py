d5210968a7ac563a34c2ef52aad930fdd0bb72f0
7044lacaba <johnlacaba6@gmail.com>
2023-10-03 15:23:41 -0700

    3

--- Neetcode150/2-Two-Pointers/3-3-Sum.py
+++ Neetcode150/2-Two-Pointers/3-3-Sum.py
@@ -4,4 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        nums.sort()
+        final = []
+
+        for i, n in enumerate(nums):
+            l = (i + 1)
+            r = (len(nums) - 1)
+            if l == r:
+                break
+            opp = 0 - n
+            while l != r and l < r:
+                sum = nums[l] + nums[r]
+                if sum < opp:
+                    l += 1
+                elif sum > opp:
+                    r -= 1
+                else:
+                    add = [n, nums[l], nums[r]]
+                    if add not in final:
+                        final.append(add)
+                    l += 1
+                    r -= 1
+        return(final)
+
+# nums = [-1,0,1,2,-1,-4]
+# s = Solution()
+# print(s.threeSum(nums))
--- Neetcode150/2-Two-Pointers/test.py
+++ Neetcode150/2-Two-Pointers/test.py
@@ -1,26 +1,6 @@
-numbers = [2,3,4]
-target = 6
 
-index_1 = 0
-index_2 = 1
+list = [[-2,0,2],[-2,0,2]]
 
-dict = {}
+list_2 = [2,2,3]
 
-while index_1 != index_2:
-
-    # Add first into dict
-    value = target - numbers[index_1]
-    dict[value] = index_1
-
-    # Check item at index 2
-    if numbers[index_2] in dict:
-        ret = [(dict[numbers[index_2]] + 1), (index_2 +1)]
-        print(ret)
-    
-    # Increment
-    if index_2 + 1 == len(numbers):
-        index_1 += 1
-    else:
-        index_1 += 1
-        index_2 += 1
-
+print(set(list))
