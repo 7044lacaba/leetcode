2417f3d7b7ab70302aa09dd0019db84b6e6ca2a9
7044lacaba <johnlacaba6@gmail.com>
2023-10-06 17:03:22 -0700

    5

--- Neetcode150/2-Two-Pointers/5-Trappping-Rain-Water.py
+++ Neetcode150/2-Two-Pointers/5-Trappping-Rain-Water.py
@@ -5,3 +5,43 @@ class Solution(object):
         :rtype: int
         """
         
+        # find start
+
+        start = 0
+        for i, n in enumerate(height):
+            if n > height[i + 1]:
+                start = i
+                break
+        
+        # keep moving right pointer untill increase/equal is found 
+
+        l_pntr = start
+        r_pntr = start
+        prev_val = 0
+
+        max = height[l_pntr]
+
+        convert_list = []
+        middle = 0 
+
+        while r_pntr < (len(height) - 1):
+            
+            # move right pointer
+
+            r_pntr += 1
+
+            # check for increase 
+
+
+            # if next bar increases then move left and calculate the inside 
+            if height[r_pntr] >= height[l_pntr]:
+                # for item in convert list find the water value and add then up
+                # clear convert list 
+                pass
+                # l_pntr = r_pntr
+            else:
+                convert_list.append(height[])
+    def convert(self, list, convert_val):
+        total = 0
+        for item in list:
+            
