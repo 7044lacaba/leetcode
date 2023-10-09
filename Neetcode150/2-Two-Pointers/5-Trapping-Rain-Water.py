8aac5d4ee9446012af3e6485c0af0db061cd5699
7044lacaba <johnlacaba6@gmail.com>
2023-10-09 02:11:19 -0700

    init

--- Neetcode150/2-Two-Pointers/5-Trapping-Rain-Water.py
+++ Neetcode150/2-Two-Pointers/5-Trapping-Rain-Water.py
@@ -6,16 +6,11 @@ class Solution(object):
         """
         l_ptr = 0
         r_ptr = 0
-
         second = [0,0]
         water = 0
         prev_value = 0
-
         while r_ptr != len(height):
-
             # check if previous value is 
-
-
             # check if valid right wall is found
             if height[r_ptr] >= height[l_ptr]:
 
@@ -62,7 +57,26 @@ class Solution(object):
         return total_water
     
 
-    
+class Solution:
+    def trap(self, height):
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
+
+
 height = [5,4,1,2]
 s = Solution()
 print(s.trap(height))
