cc946fcf88277ff470f00f472d3bb79d34c2cd98
7044lacaba <johnlacaba6@gmail.com>
2023-10-10 18:20:34 -0700

    2

---
+++ Neetcode150/3-Sliding-Window/2-Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l, r = 0, 0
+        maxlen = 0
+
+        while r < len(s):
+            temp = s[l:r]
+            if len(temp) > maxlen:
+                maxlen = len(temp)
+            if s[r] in temp:
+                while True:
+                    if s[l] == s[r]:
+                        l += 1
+                        break
+                    else:
+                        l += 1
+            else: 
+                r += 1
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+str = " "
+s = Solution()
+print(s.lengthOfLongestSubstring(str))
