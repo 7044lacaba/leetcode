31d27d1aa439e236b281b9f76125d33d6529ba8e
7044lacaba <johnlacaba6@gmail.com>
2023-10-14 14:39:14 -0700

    4

--- Neetcode150/3-Sliding-Window/4-Permutation-in-String.py
+++ Neetcode150/3-Sliding-Window/4-Permutation-in-String.py
@@ -1,47 +1,71 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        first = s1[0]
+        l = 0
+        r = len(s1) - 1
 
-        for i, c in enumerate(s2):
-            if first == c:
+        s1_count = {}
+        s2_count = {}
 
-                copy = [*s1]
-                # remove current from copy
-                copy.remove(c)
-                if len(copy) == 0:
-                    return True
+        copy = [*s1]
+        for i in range(len(copy)):
+            s1_count[s1[i]] = s1_count.get(s1[i], 0) + 1
+            s2_count[s2[i]] = s2_count.get(s2[i], 0) + 1
+        
+        while True:
+            if s1_count == s2_count:
+                return True
 
-
-                l = i - 1
-                r = i + 1
-
-                if l < 0:
-                    left = 1
-                else:
-                    left = s2[l]
-                if r >= len(s2):
-                    right = 1
-                else:
-                    right = s2[r]
-
-                while True:
-                    if left not in copy and right not in copy:
-                        break
-                    if left in copy:
-                        copy.remove(left)
-                        if len(copy) == 0:
-                            return True
-                        l -= 1
-                        left = s2[l]
-
-                    if right in copy:
-                        copy.remove(right)
-                        if len(copy) == 0:
-                            return True
-                        r += 1
-                        right = s2[r]
-
+            if s2[l] in s2_count:
+                s2_count[s2[l]] -= 1
+                if s2_count[s2[l]] == 0:
+                    del s2_count[s2[l]]
+            else:
+                del s2_count[s2[l]]
+            l += 1
+            r += 1
+            if not r < len(s2):
+                break
+            s2_count[s2[r]] = s2_count.get(s2[r], 0) + 1
+            
         return False
+    
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord("a")] += 1
+            s2Count[ord(s2[i]) - ord("a")] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += 1 if s1Count[i] == s2Count[i] else 0
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            index = ord(s2[r]) - ord("a")
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord("a")
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
+
+
 
 s1 = "mart"
 s2 = "karma"
--- test.py
+++ test.py
@@ -1,5 +1,38 @@
-string = "hello"
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        r = len(s1) - 1
 
-e = [*string]
+        s1_count = {}
+        s2_count = {}
 
-print(e)
+        copy = [*s1]
+        for i in range(len(copy)):
+            s1_count[s1[i]] = s1_count.get(s1[i], 0) + 1
+            s2_count[s2[i]] = s2_count.get(s2[i], 0) + 1
+        
+        while True:
+            if s1_count == s2_count:
+                return True
+
+            if s2[l] in s2_count:
+                s2_count[s2[l]] -= 1
+                if s2_count[s2[l]] == 0:
+                    del s2_count[s2[l]]
+            else:
+                del s2_count[s2[l]]
+            l += 1
+            r += 1
+            if not r < len(s2):
+                break
+            s2_count[s2[r]] = s2_count.get(s2[r], 0) + 1
+            
+        return False
+
+
+
+s1 = "adc"
+s2 = "dcda"
+
+s = Solution()
+print(s.checkInclusion(s1, s2))
