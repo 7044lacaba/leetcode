d09934b041c13cb41a9216a1d4c9715f210f6d88
7044lacaba <johnlacaba6@gmail.com>
2023-10-16 14:49:47 -0700

    6 init

--- test.py
+++ test.py
@@ -1,38 +1,24 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def minWindow(self, s: str, t: str) -> str:
+
+        s_count = {}
+        t_count = {}
         l = 0
-        r = len(s1) - 1
+        r = 0
 
-        s1_count = {}
-        s2_count = {}
-
-        copy = [*s1]
-        for i in range(len(copy)):
-            s1_count[s1[i]] = s1_count.get(s1[i], 0) + 1
-            s2_count[s2[i]] = s2_count.get(s2[i], 0) + 1
+        for c in t:
+            c.lower()
+            t_count[c] = t_count.get(c, 0) + 1
         
-        while True:
-            if s1_count == s2_count:
-                return True
 
-            if s2[l] in s2_count:
-                s2_count[s2[l]] -= 1
-                if s2_count[s2[l]] == 0:
-                    del s2_count[s2[l]]
-            else:
-                del s2_count[s2[l]]
-            l += 1
-            r += 1
-            if not r < len(s2):
-                break
-            s2_count[s2[r]] = s2_count.get(s2[r], 0) + 1
-            
-        return False
+        
+        return t_count
 
 
 
-s1 = "adc"
-s2 = "dcda"
+
+s = "ADOBECODEBANC" 
+t = "ABC"
 
 s = Solution()
-print(s.checkInclusion(s1, s2))
+print(s.minWindow(s, t))
