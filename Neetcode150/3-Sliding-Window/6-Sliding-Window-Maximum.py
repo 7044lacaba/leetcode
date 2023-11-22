5329e84ca7fbb51c4305b085c8e4f20679f6e788
7044lacaba <johnlacaba6@gmail.com>
2023-11-21 18:20:59 -0800

    3.6

--- Neetcode150/3-Sliding-Window/6-Sliding-Window-Maximum.py
+++ Neetcode150/3-Sliding-Window/6-Sliding-Window-Maximum.py
@@ -1,19 +1,30 @@
+# class Solution:
+#     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+#         max_list = []
+#         l = 0 
+#         r = k
+#         while r < len(nums) + 1:
+#             window = nums[l:r]
+#             window.sort()
+#             max_list.append(window[k - 1])
+#             l += 1
+#             r += 1
+#         return max_list
+
+
+# nums = [1,3,-1,-3,5,3,6,7]
+# k = 3
+
+
+
+
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
-        max_list = []
-        l = 0 
-        r = k
-        while r < len(nums) + 1:
-            window = nums[l:r]
-            window.sort()
-            max_list.append(window[k - 1])
-            l += 1
-            r += 1
-        return max_list
+
+        pass
 
 
-nums = [1,3,-1,-3,5,3,6,7]
-k = 3
+
 
 
 s = Solution()
