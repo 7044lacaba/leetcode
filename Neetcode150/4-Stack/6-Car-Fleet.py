0a2214ba4eab80086722c89d7fb1e93ee41b5d57
7044lacaba <johnlacaba6@gmail.com>
2023-11-21 18:00:58 -0800

    4.6

--- Neetcode150/4-Stack/6-Car-Fleet.py
+++ Neetcode150/4-Stack/6-Car-Fleet.py
@@ -6,15 +6,53 @@ class Solution(object):
         :type speed: List[int]
         :rtype: int
         """
-        count  = 0
-        while True:
+        pos_and_speed = []
+        stack = []
 
-            for i in range(len(target)):
-                position[i] = position[i] + speed[i]
+        # for i in range(len(position)):
+        #     pos_and_speed.append([position[i], speed[i]])
+
+        print(zip(position, speed))
+
+        for p, s in zip(position, speed):
+            pos_and_speed.append([p, s])
+        
+        print(pos_and_speed)
+        print(sorted(pos_and_speed))
+
+        for pair in sorted(pos_and_speed)[::-1]:
+            time = (target - pair[0]) / pair[1]
+            stack.append(time)
+
+
+            # you pop the faster value because itll get stuck behind the slower car (remmeber we're iterating in reverse order)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
 
 
 
+# class Solution:
+#     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+#         pair = [(p, s) for p, s in zip(position, speed)]
+#         pair.sort(reverse=True)
+#         stack = []
+#         for p, s in pair:  # Reverse Sorted Order
+#             stack.append((target - p) / s)
+#             if len(stack) >= 2 and stack[-1] <= stack[-2]:
+#                 stack.pop()
+#         return len(stack)
 
 
 
-# zip 
+# zip to to iterate through two list of same len
+# or you can use a hash map and sort by position
+
+
+target = 10
+position = [0,4,2]
+speed = [2,1,3]
+
+s = Solution()
+print(s.carFleet(target, position, speed))
