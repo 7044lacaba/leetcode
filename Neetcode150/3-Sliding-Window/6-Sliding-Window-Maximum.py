828d41427b31a64e3a5d986a8fcff492879b9b36
7044lacaba <johnlacaba6@gmail.com>
2023-11-24 15:41:26 -0800

    3.6

--- Neetcode150/3-Sliding-Window/6-Sliding-Window-Maximum.py
+++ Neetcode150/3-Sliding-Window/6-Sliding-Window-Maximum.py
@@ -12,8 +12,7 @@
 #         return max_list
 
 
-# nums = [1,3,-1,-3,5,3,6,7]
-# k = 3
+
 
 
 
@@ -21,12 +20,41 @@
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
 
-        pass
+        l = 0 
+        r = k - 1
+
+        stack = []
+        final = []
+
+        # prep everything to starting position 
+        for temp_r in range(0, k - 1):
+            if len(stack) == 0:
+                stack.append(nums[temp_r])
+            elif nums[r] > stack[-1]:
+                stack.pop(-1)
+                stack.append(nums[temp_r])
+            else:
+                stack.append(nums[temp_r])
+        
+        # loop through rest of nums 
+        while r < len(nums):
+            
+            if nums[r] > stack[-1]:
+                stack.pop(-1)
+                stack.append(nums[temp_r])
+            else:
+                stack.append(nums[temp_r])
+            
+            final.append(stack[0])
+
+            if nums[r] == stack[0]:
+                stack.pop(0)
 
 
 
 
-
+nums = [1,3,-1,-3,5,3,6,7]
+k = 3
 s = Solution()
 print(s.maxSlidingWindow(nums, k))
 
